4163662b8f7e5fb00911d85d5d1febaf8d275552
weiyu <yin1532776643@gmail.com>
2026-09-21 20:25:58 -0500

    test: try to buy one item

--- SellingTest/SellingTests.cs
+++ SellingTest/SellingTests.cs
@@ -5,6 +5,20 @@ namespace SellingTest
 {
     public class SellingTests
     {
+        private readonly InventoryOrderService _service = new();
+        // test 1 : buy 1 item
+        [Fact]
+        public void Buy1Item()
+        {
+            // Arrange
+            _service.AddProduct(new Product { Id = "1", Name = "Iphone ProMax 18", UnitPrice = 2399m, StockQuantity = 1000 });
 
+            // Act
+            var res = _service.ProcessOrder("1", 1, 0);
+
+            // Assert
+            Assert.True(res.IsSuccess);
+            Assert.Equal(999, _service.GetProduct("1")!.StockQuantity);
+        }
     }
 }
